447489cb614dac5d66fc8977f1d94d41f25c75dc
Leandro Argentiero <argentiero.leo@gmail.com>
2024-08-20 19:26:46 +0200

    test: added case for when sell date has passed

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -14,4 +14,20 @@ describe('Gilded Rose', () => {
     expect(items[0].sellIn).toBe(0);
     expect(items[0].quality).toBe(0);
   });
+
+  it('quality should decrease 2x when sell date has passed', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 4)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe('foo');
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(2);
+  });
+
+  it('quality should never be negative', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe('foo');
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(0);
+  });
 });
